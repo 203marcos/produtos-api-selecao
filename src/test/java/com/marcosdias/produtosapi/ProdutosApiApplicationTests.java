d680fd04db507b8d16bb2bdfd667bf34cdc5c150
Marcos Dias <marcosaurelioce@hotmail.com>
2026-03-24 15:17:23 -0300

    chore(config): adicionar perfis dev/test/prod e pipeline de CI

--- src/test/java/com/marcosdias/produtosapi/ProdutosApiApplicationTests.java
+++ src/test/java/com/marcosdias/produtosapi/ProdutosApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.marcosdias.produtosapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProdutosApiApplicationTests {
 
     @Test
